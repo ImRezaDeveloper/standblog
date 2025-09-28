c74a29efbe2e3c8d11fb7927b7fc9de0f0c24eb6
Reza <rezapapi138418@gmail.com>
2025-09-28 23:59:49 +0330

    fix some bog that occured in our app

--- standblog_project/blog_app/models.py
+++ standblog_project/blog_app/models.py
@@ -3,8 +3,12 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class ArticleModel(models.Model):
+    CHOISES = (
+        ('A', 'Python'),
+        ('B', 'Django')
+    )
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author_article', null=True, blank=True, unique=True)
-    title = models.CharField(max_length=40)
+    title = models.CharField(max_length=40, choices=CHOISES)
     body = models.TextField(help_text='Enter a valid body because important!')
     image = models.ImageField(upload_to='media/blogs', null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True)
