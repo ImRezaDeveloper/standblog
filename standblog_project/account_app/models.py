f9994201ea36f59857f5c2bff90b30837bee67dc
Reza <rezapapi138418@gmail.com>
2025-09-30 00:18:37 +0330

    add profile model to account_app

--- standblog_project/account_app/models.py
+++ standblog_project/account_app/models.py
@@ -1,4 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    father_s_name = models.CharField(max_length=30)
+    melicode = models.IntegerField(max_length=20)
+    image = models.ImageField(upload_to='profiles/image')
+    
+    def __str__(self):
+        return self.user.username
