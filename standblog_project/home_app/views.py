85ebc8be413107b2d2e0ecf41dc759051aeee137
Reza <rezapapi138418@gmail.com>
2025-09-30 00:04:24 +0330

    add category model to blog_app

--- standblog_project/home_app/views.py
+++ standblog_project/home_app/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-from blog_app.models import ArticleModel
+from blog_app.models import ArticleModel, Category
 
 # Create your views here.
 
 def home(request):
     articles = ArticleModel.objects.all()
-    return render(request, 'home_app/index.html', {'articles': articles})
+    categories = Category.objects.all()
+    return render(request, 'home_app/index.html', {'articles': articles, 'category': categories})
