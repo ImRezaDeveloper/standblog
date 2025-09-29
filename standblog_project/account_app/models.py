92ac0358443e7d3921fc169112420af84c648542
Reza <rezapapi138418@gmail.com>
2025-09-30 00:40:53 +0330

    change the image concept in account model

--- standblog_project/account_app/models.py
+++ standblog_project/account_app/models.py
@@ -7,7 +7,7 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     father_s_name = models.CharField(max_length=30)
     melicode = models.IntegerField(max_length=20)
-    image = models.ImageField(upload_to='profiles/image')
+    image = models.ImageField(upload_to='profiles/image', null=True, blank=True)
     
     def __str__(self):
         return self.user.username
