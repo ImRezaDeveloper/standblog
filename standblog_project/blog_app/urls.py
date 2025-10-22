6edadc7c980febed10186b1a603716f8eacef8e2
Reza <rezapapi138418@gmail.com>
2025-10-22 22:35:39 +0330

    finish some class based view in django

--- standblog_project/blog_app/urls.py
+++ standblog_project/blog_app/urls.py
@@ -3,9 +3,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('article/<slug:slug>/', views.post_detail, name='single-article'),
-    path('articles/list', views.article_list, name='articles'),
+    path('article/<slug:slug>/', views.ArticleDetail.as_view(), name='single-article'),
+    path('articles/list', views.ArticleList.as_view(), name='articles'),
     path('category/<int:pk>', views.categories_articles, name='category_article'),
     path('search/', views.search_article, name='search_article'),
-    path('contact-us/', views.contact_us, name='contact_us')
+    # path('contact-us/', views.ContactUsView.as_view(), name='contact_us'),
+    path('contact-us/', views.MessageView.as_view(), name='contact_us'),
+    path('redirect', views.HomePageRedirectView.as_view(), name='redirect')
 ]
--- standblog_project/blog_app/views.py
+++ standblog_project/blog_app/views.py
@@ -3,6 +3,11 @@ from .models import ArticleModel, Category, Comment, ContactUs
 from django.core.paginator import Paginator
 from .forms import ContactUsForms
 from django.db.models import Count
+from django.views.generic.base import TemplateView, RedirectView
+# from django.views.generic.list import Crea
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import FormView, CreateView
+from django.urls import reverse_lazy
 
 # Create your views here.
 
@@ -56,3 +61,72 @@ def contact_us(request):
     else:
         form = ContactUsForms()
     return render(request, 'blog_app/contact_us.html', {'form': form})
+
+# class ArticleList(ListView):
+#     queryset = ArticleModel.objects.all()
+#     template_name = 'blog_app/article_list.html'
+    
+#     def get(self, request):
+#         page = request.GET.get('page')
+#         paginator = Paginator(self.queryset, 1)
+#         object_list = paginator.get_page(page)
+#         return render(request, self.template_name, {'articles': object_list})
+
+class ArticleList(TemplateView):
+    
+    template_name = 'blog_app/article_list.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['articles'] = ArticleModel.objects.all()
+        return context
+    
+class HomePageRedirectView(RedirectView):
+    # url = '/'
+    pattern_name = 'home'
+    
+class ArticleDetail(DetailView):
+    model = ArticleModel
+    template_name = 'blog_app/post-details.html'
+    
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        queryset = ArticleModel.objects.get(slug=self.kwargs['slug'])
+        return queryset
+    
+    def get_context_data(self, **kwargs,):
+        context = super().get_context_data(**kwargs)
+        context["article"] = ArticleModel.objects.get(slug=self.kwargs['slug'])
+        return context
+    
+    def post(self, request):
+        body = request.POST.get('body')
+        parent_id = request.POST.get('parent_id')
+        Comment.objects.create(body=body, article=self.queryset, user=request.user, parent_id=parent_id)
+        
+class ContactUsView(FormView):
+    template_name = 'blog_app/contact_us.html'
+    form_class = ContactUsForms
+    success_url = reverse_lazy('home')
+    
+    def form_valid(self, form):
+        form = form.cleaned_data
+        ContactUs.objects.create(**form)
+        return super().form_valid(form)
+
+class MessageView(CreateView):
+    model = ContactUs
+    fields = ('title', 'text')
+    success_url = reverse_lazy('home')
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['messages'] = ContactUs.objects.all()
+        return context
+    
+    def form_valid(self, form):
+        instance = form.save(commit=False)
+        instance.email = self.request.user.email
+        instance.save()
+        return super().form_valid(form)
+        
